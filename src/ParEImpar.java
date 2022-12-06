c2ad9ebaabb9e69915bd1337cf2125f8db727ea9
melissarsousa <melissa.rios.sousa@gmail.com>
2022-12-06 14:31:24 -0300

    adicionando nova classe ParEImpar.java

---
+++ src/ParEImpar.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+public class ParEImpar {
+
+public static void main(String[] args){
+
+    Scanner scan = new Scanner(System.in);
+
+    int quantidadeNumero = 0;
+    int quantidadePar = 0;
+    int quantidadeImpar = 0;
+    int numero = 0;
+
+    System.out.println("Insira a quantidade de números inteiros: ");
+    quantidadeNumero = scan.nextInt();
+    int count = 0;
+
+    do {
+        System.out.println("Número: ");
+            numero = scan.nextInt();
+            if (numero % 2 == 0) quantidadePar++;
+            else quantidadeImpar++;
+
+            count++;
+
+    } while (count < quantidadeNumero);
+
+    System.out.println("A quantidade de números pares e ímpares é, respectivamente: " + quantidadePar + ", " + quantidadeImpar + ".");
+
+    scan.close();
+}
+
+}
+
+
