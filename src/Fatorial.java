4686cfcdf36b67e27aa1d4f78d72544ffe7e848a
melissarsousa <melissa.rios.sousa@gmail.com>
2022-12-06 15:05:33 -0300

    adicionando classes Tabuada e Fatorial

---
+++ src/Fatorial.java
@@ -0,0 +1,23 @@
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 
+*/
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um número para calcular o fatorial: ");
+        int numero = scan.nextInt();
+        int multiplicacao = 1;
+
+    for (int i=numero;i>=1; i--){
+        multiplicacao = multiplicacao*i;
+    }
+    System.out.println(numero+"! = "+multiplicacao);
+
+    scan.close();
+    }
+
+}
