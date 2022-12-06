83160129dd6150e1e9c73e009c994dae7c63f2ce
melissarsousa <melissa.rios.sousa@gmail.com>
2022-12-06 11:03:52 -0300

    Criando classe MaiorEMedia

---
+++ src/MaiorEMedia.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MaiorEMedia {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero = 0;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Digite um número: ");
+            numero = scan.nextInt();
+            soma = soma + numero;
+
+            if (numero > maior)
+                maior = numero;
+
+            count = count + 1;
+
+        } while (count < 5);
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Média dos números: " + (soma / count));
+
+        scan.close();
+
+    }
+}
